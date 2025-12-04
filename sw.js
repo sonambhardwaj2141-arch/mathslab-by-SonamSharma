857ff1e0cdb9877f3315b1a422ecadbbfc2526f6
sonambhardwaj2141-arch <sonambhardwaj2141@gmail.com>
2025-12-04 16:58:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,11 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+// 🚀 Force immediate update
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
 
-const CACHE = "trigno-v10";  // 🔥 CHANGE THIS VERSION NUMBER
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./refresh.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./images/gallery_1.jpeg",
-  "./images/gallery_2.jpeg",
-  "./images/gallery_3.jpeg",
-  "./images/gallery_4.jpeg",
-  "./images/gallery_5.jpeg",
-  "./images/gallery_6.jpeg",
-  "./images/gallery_7.jpeg",
-  "./images/gallery_8.jpeg",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      // Delete ALL old caches
+      const keys = await caches.keys();
+      await Promise.all(keys.map((key) => caches
